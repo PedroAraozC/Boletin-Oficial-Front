e99f7e08afd82af585ceea086e2f219f422daf4d
Pedro Aráoz <pedroaraozc@gmail.com>
2025-04-07 09:35:28 -0300

    se modificó axios para control de token

--- src/config/axios.js
+++ src/config/axios.js
@@ -9,6 +9,21 @@ export const axios = axiosOriginal.create({
   baseURL: "https://estadisticas.smt.gob.ar:5557",
 });
 
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response && error.response.status === 401) {
+      console.log('Token inválido o expirado (401)');
+      localStorage.clear();
+      const url = new URL('https://ciudaddigital.smt.gob.ar/');
+      url.searchParams.append("logout", true);
+      window.open(url.toString(), '_self');
+    }
+    return Promise.reject(error);
+  }
+);
+
+
 export const axiosDigital = axiosOriginal.create({
   baseURL: "https://estadisticas.smt.gob.ar:5557",
   // baseURL: "https://ciudaddigital.smt.gob.ar:2000", // BACK-DERIVADOR
@@ -16,3 +31,16 @@ export const axiosDigital = axiosOriginal.create({
   // baseURL: "https://boletinoficial.smt.gob.ar:5557" //SERVIDOR DE PRODUCCION
 
 });
+axiosDigital.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response && error.response.status === 401) {
+      console.log('Token inválido o expirado (401)');
+      localStorage.clear();
+      const url = new URL('https://ciudaddigital.smt.gob.ar/');
+      url.searchParams.append("logout", true);
+      window.open(url.toString(), '_self');
+    }
+    return Promise.reject(error);
+  }
+);
